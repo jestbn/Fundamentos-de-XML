6f525483ae4015a8926d4679c5dc36a88ad2441f
Juan Esteban Gil <juanestebang9@gmail.com>
2019-05-02 21:33:14 -0500

    Initial commit

--- Fundamentos de XML/Form1.Designer.cs
+++ Fundamentos de XML/Form1.Designer.cs
@@ -28,13 +28,294 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgEmpleados = new System.Windows.Forms.DataGridView();
+            this.dtmRetiro = new System.Windows.Forms.DateTimePicker();
+            this.dtmIngreso = new System.Windows.Forms.DateTimePicker();
+            this.cbRol = new System.Windows.Forms.ComboBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtDatosAdicionales = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnReEstablecer = new System.Windows.Forms.Button();
+            this.txtDocumento = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgEmpleados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgEmpleados
+            // 
+            this.dgEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgEmpleados.Location = new System.Drawing.Point(9, 229);
+            this.dgEmpleados.Name = "dgEmpleados";
+            this.dgEmpleados.Size = new System.Drawing.Size(611, 229);
+            this.dgEmpleados.TabIndex = 68;
+            // 
+            // dtmRetiro
+            // 
+            this.dtmRetiro.CustomFormat = "dd/mm/yy";
+            this.dtmRetiro.Location = new System.Drawing.Point(488, 69);
+            this.dtmRetiro.Name = "dtmRetiro";
+            this.dtmRetiro.Size = new System.Drawing.Size(132, 20);
+            this.dtmRetiro.TabIndex = 51;
+            // 
+            // dtmIngreso
+            // 
+            this.dtmIngreso.CustomFormat = "dd/mm/yy";
+            this.dtmIngreso.Location = new System.Drawing.Point(488, 39);
+            this.dtmIngreso.Name = "dtmIngreso";
+            this.dtmIngreso.Size = new System.Drawing.Size(132, 20);
+            this.dtmIngreso.TabIndex = 50;
+            // 
+            // cbRol
+            // 
+            this.cbRol.FormattingEnabled = true;
+            this.cbRol.Location = new System.Drawing.Point(488, 12);
+            this.cbRol.Name = "cbRol";
+            this.cbRol.Size = new System.Drawing.Size(132, 21);
+            this.cbRol.TabIndex = 49;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(546, 199);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(74, 24);
+            this.btnSalir.TabIndex = 59;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(13, 182);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(103, 41);
+            this.btnGuardar.TabIndex = 54;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // txtDatosAdicionales
+            // 
+            this.txtDatosAdicionales.Location = new System.Drawing.Point(488, 95);
+            this.txtDatosAdicionales.Multiline = true;
+            this.txtDatosAdicionales.Name = "txtDatosAdicionales";
+            this.txtDatosAdicionales.Size = new System.Drawing.Size(132, 47);
+            this.txtDatosAdicionales.TabIndex = 53;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(161, 122);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(132, 20);
+            this.txtEmail.TabIndex = 48;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(160, 93);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(132, 20);
+            this.txtTelefono.TabIndex = 47;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(161, 64);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(132, 20);
+            this.txtDireccion.TabIndex = 46;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(336, 65);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(100, 21);
+            this.label13.TabIndex = 66;
+            this.label13.Text = "Fecha retiro";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(336, 38);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(115, 21);
+            this.label10.TabIndex = 65;
+            this.label10.Text = "Fecha ingreso";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(336, 95);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(145, 21);
+            this.label11.TabIndex = 64;
+            this.label11.Text = "Datos adicionales";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(336, 10);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(35, 21);
+            this.label12.TabIndex = 63;
+            this.label12.Text = "Rol";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(9, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 21);
+            this.label7.TabIndex = 62;
+            this.label7.Text = "Email";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(8, 91);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 21);
+            this.label8.TabIndex = 61;
+            this.label8.Text = "Teléfono";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(9, 63);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(83, 21);
+            this.label9.TabIndex = 60;
+            this.label9.Text = "Direccion";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(122, 182);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(103, 41);
+            this.btnEliminar.TabIndex = 57;
+            this.btnEliminar.Text = "Borrar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnReEstablecer
+            // 
+            this.btnReEstablecer.Location = new System.Drawing.Point(231, 182);
+            this.btnReEstablecer.Name = "btnReEstablecer";
+            this.btnReEstablecer.Size = new System.Drawing.Size(103, 41);
+            this.btnReEstablecer.TabIndex = 58;
+            this.btnReEstablecer.Text = "Nuevo";
+            this.btnReEstablecer.UseVisualStyleBackColor = true;
+            // 
+            // txtDocumento
+            // 
+            this.txtDocumento.Location = new System.Drawing.Point(161, 37);
+            this.txtDocumento.Name = "txtDocumento";
+            this.txtDocumento.Size = new System.Drawing.Size(132, 20);
+            this.txtDocumento.TabIndex = 45;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(161, 11);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(132, 20);
+            this.txtNombre.TabIndex = 44;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 21);
+            this.label3.TabIndex = 55;
+            this.label3.Text = "Documento";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 21);
+            this.label2.TabIndex = 52;
+            this.label2.Text = "Nombre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(626, 467);
+            this.Controls.Add(this.dgEmpleados);
+            this.Controls.Add(this.dtmRetiro);
+            this.Controls.Add(this.dtmIngreso);
+            this.Controls.Add(this.cbRol);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtDatosAdicionales);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnReEstablecer);
+            this.Controls.Add(this.txtDocumento);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Empleado";
+            ((System.ComponentModel.ISupportInitialize)(this.dgEmpleados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgEmpleados;
+        private System.Windows.Forms.DateTimePicker dtmRetiro;
+        private System.Windows.Forms.DateTimePicker dtmIngreso;
+        private System.Windows.Forms.ComboBox cbRol;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtDatosAdicionales;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnReEstablecer;
+        private System.Windows.Forms.TextBox txtDocumento;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
